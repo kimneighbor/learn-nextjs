6517a5ee123fb54fa1ce9748d322b17e28eee51a
kimneighbor <designkimdh@gmail.com>
2023-08-29 13:28:15 +0900

    test backend

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -1,8 +1,14 @@
-export default function Read (props:any) {
+export default async function Read (props:any) {
+    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+    const topic = await resp.json();
+
     return(
         <>
-        <h2>read/[id]/page.tsx</h2>
-            parameters : {props.params.id}
+        {/*<h2>read/[id]/page.tsx</h2>*/}
+            <h2>{topic.title}</h2>
+            {/*데이터 출력*/}
+            {topic.body}
+            {/*parameters : {props.params.id}*/}
         </>
     )
 }
