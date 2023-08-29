d46b55b948f2e68a54ac743a200e8c75094c4382
kimneighbor <designkimdh@gmail.com>
2023-08-29 16:28:24 +0900

    add delete

--- src/app/Control.tsx
+++ src/app/Control.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import Link from "next/link";
-import {useParams} from "next/navigation";
+import {useParams, useRouter} from "next/navigation";
 
 export function Control() {
     const params = useParams();
+    const router = useRouter();
     console.log(params);
     const id = params.id;
     return (
@@ -12,7 +13,16 @@ export function Control() {
             <li><Link href="/create">Create</Link></li>
             {id ? <>
                 <li><Link href={`/update/`+id}>Update</Link></li>
-                <li><input type="button" value="delete"/></li>
+                <li><input type="button" value="delete" onClick={() => {
+                    const options = {method: 'DELETE'}
+                    fetch('http://localhost:9999/topics/' + id, options)
+                        .then(resp => resp.json())
+                        .then(result => {
+                            router.push('/');
+                            //캐시 리프레시
+                            router.refresh();
+                        })
+                }}/></li>
                 </> : null}
         </ul>
     )
